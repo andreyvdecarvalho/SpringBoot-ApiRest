861c295bb90cf3d74bfd3fcc10fc46bbf49a59c3
Andrey <ay.carvalho@hotmail.com>
2025-11-25 14:13:34 -0300

    padronizando retornos API

--- src/main/java/med/oak/api/controller/MedicoController.java
+++ src/main/java/med/oak/api/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,22 +25,28 @@ public class MedicoController {
 
     @GetMapping
     @Transactional
-    public Page<DadosListagemMedicos> listar(@PageableDefault(sort = {"nome"}) Pageable paginacao){
-        return repository.findByAtivoTrue(paginacao).map(DadosListagemMedicos::new);
+    public ResponseEntity<Page<DadosListagemMedicos>> listar(@PageableDefault(sort = {"nome"}) Pageable paginacao){
+        var page = repository.findByAtivoTrue(paginacao).map(DadosListagemMedicos::new);
+
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizarMedico atualizarMedico){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizarMedico atualizarMedico){
         var medico = repository.getReferenceById(atualizarMedico.id());
         medico.atualizarDados(atualizarMedico);
+
+        return ResponseEntity.ok(new DadosDetalhadosMedico(medico));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void deletar(@PathVariable Long id){
+    public ResponseEntity deletar(@PathVariable Long id){
         var medico = repository.getReferenceById(id);
         medico.deletar();
+
+        return ResponseEntity.noContent().build();
     }
 
 }
---
+++ src/main/java/med/oak/api/medico/DadosDetalhadosMedico.java
@@ -0,0 +1,10 @@
+package med.oak.api.medico;
+
+import med.oak.api.endereco.Endereco;
+
+public record DadosDetalhadosMedico(Long id, String nome, String email, String telefone, String crm, Especialidade especialidade, Endereco endereco) {
+
+    public DadosDetalhadosMedico(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
+    }
+}
