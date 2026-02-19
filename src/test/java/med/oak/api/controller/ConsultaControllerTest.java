ea5f84d6c804a5de432aac5b62f1374239121220
Andrey <ay.carvalho@hotmail.com>
2026-02-18 22:06:07 -0300

    realizado teste controller

--- src/test/java/med/oak/api/controller/ConsultaControllerTest.java
+++ src/test/java/med/oak/api/controller/ConsultaControllerTest.java
@@ -1,23 +1,45 @@
 package med.oak.api.controller;
 
+import med.oak.api.domain.consulta.AgendaConsulta;
+import med.oak.api.domain.consulta.DadosAgendarConsulta;
+import med.oak.api.domain.consulta.DadosDetalhadosAgendamento;
+import med.oak.api.domain.medico.Especialidade;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class ConsultaControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private JacksonTester<DadosAgendarConsulta> dadosAgendamentoJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhadosAgendamento> dadosDetalhamentoAgendamentoJson;
+
+    @MockBean
+    private AgendaConsulta agendaConsulta;
 
     @Test
     @DisplayName("Deveria devolver 400 de resposta devido informações invalidas")
@@ -27,5 +49,33 @@ class ConsultaControllerTest {
                 .andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("Deveria devolver 200 de resposta devido informações invalidas")
+    @WithMockUser
+    void agendarCenario2() throws Exception {
+        var data = LocalDateTime.now().plusHours(1);
+        var especialidade = Especialidade.CARDIOLOGIA;
+
+        var dadosDetalhamento = new DadosDetalhadosAgendamento(null, 2l,  5l, data);
+
+        when(agendaConsulta.agendar(any())).thenReturn(dadosDetalhamento);
+
+        var response = mockMvc.perform(
+                post("/consultas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(dadosAgendamentoJson.write(
+                                new DadosAgendarConsulta(2l, 5l, data, especialidade)
+                        ).getJson())
+                ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        var jsonEsperado = dadosDetalhamentoAgendamentoJson.write(
+                dadosDetalhamento
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(jsonEsperado);
     }
 }
